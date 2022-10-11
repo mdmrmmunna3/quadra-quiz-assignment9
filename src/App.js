829978dfaea9ea04262b9062f37661c5806b4f36
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-10-11 12:26:43 +0600

    create some route and layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './layouts/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path:'/',
+          element: <Home></Home>
+        },
+        {
+          path:'/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path : '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    }
+  ])
   return (
     <div >
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
