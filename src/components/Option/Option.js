4f62173043bec7ac3c67247c11b505af7e49e6b2
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-10-12 20:50:58 +0600

    show eye icon and correct answer

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Option = ({ option }) => {
     return (
         <div className='flex justify-center '>
-                <div className='w-3/4 md:w-3/4 lg:w-2/4 mb-5 py-2 rounded-lg' style={{backgroundColor:'#0F3443'}}>
-                    <p className='text-center text-white font-semibold'>{option}</p>
-                </div>
+            <div className='w-3/4 md:w-3/4 lg:w-2/4 mb-5 py-2 rounded-lg' style={{ backgroundColor: '#0F3443' }}>
+                <p className='text-center text-white font-semibold'>{option}</p>
+            </div>
         </div>
     );
 };
--- src/components/QuizQuesAns/QuizQuesAns.js
+++ src/components/QuizQuesAns/QuizQuesAns.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid';
+
+import { Link } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Option from '../Option/Option';
 
 
 const QuizQuesAns = ({ quizQuesList }) => {
     const { index, question, correctAnswer, options } = quizQuesList;
-    
+    const showToastMessage = () => {
+        toast.success(correctAnswer, {
+            position: toast.POSITION.TOP_CENTER
+        });
+    };
+
     return (
         <div className='mt-6 lg:mx-16 border border-gray-300 rounded'>
             <p className='text-center mt-4 mb-5'>{question}</p>
 
-            <div className=''>
+            <div>
+                <div className='relative mb-4'>
+                    <div className=' absolute right-0'>
+                  
+                        <EyeIcon onClick={showToastMessage} className='text-4xl h-6'></EyeIcon> 
+            
+                    <ToastContainer />
+                        {/* <EyeIcon className='h-6'>{correctAnswer}</EyeIcon> */}
+                    </div>
+                </div>
+
                 {
                     options.map(option => <Option
                         key={option[index]}
                         option={option}></Option>)
                 }
+
             </div>
 
         </div>
@@ -22,3 +42,11 @@ const QuizQuesAns = ({ quizQuesList }) => {
 };
 
 export default QuizQuesAns;
+
+{/* <button> <EyeIcon onClick={showToastMessage} className='text-4xl'></EyeIcon> </button>
+                <ToastContainer />
+                const showToastMessage = () => {
+                    toast.success(correctAnswer, {
+                        position: toast.POSITION.TOP_CENTER
+                    });
+                }; */}
